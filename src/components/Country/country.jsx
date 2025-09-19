b67b2ff45095115d511a9f3dcddafbb5096ef51f
shihab1010 <shihabhossen185@gmail.com>
2025-09-19 19:04:56 +0600

    add visited not visited interactivity

--- src/components/Country/country.jsx
+++ src/components/Country/country.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Countries from "../countries/countries";
 import "./country.css";
 
-const Country = ({ country }) => {
-  console.log(country);
+const Country = ({ country, handleVisitedCountry }) => {
+  // console.log(country);
+  const [visited, setVisited] = useState(false);
+  // console.log(handleVisitedCountry);
+
+  const handleVisited = () => {
+    if (visited) {
+      setVisited(false);
+    } else {
+      setVisited(true);
+    }
+    handleVisitedCountry(country);
+  };
 
   return (
-    <div className="country">
+    <div className={`country  ${visited && "country-visited"}`}>
       <img src={country.flags.flags.png} alt="" />
       <h4>Name: {country.name.common} </h4>
       <h5>Population: {country.population.population}</h5>
@@ -14,6 +25,9 @@ const Country = ({ country }) => {
         Area: {country.area.area}{" "}
         {country.area.area > 300000 ? "Big Country" : "Small Country"}
       </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "../Country/country";
 import "./countries.css";
 
@@ -6,12 +6,29 @@ const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
 
+  const [visitedCountries, setVisitedCountry] = useState([]);
+
+  const handleVisitedCountry = (country) => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountry(newVisitedCountries);
+  };
+
   return (
     <div>
       <h1>Countries: {countries.length}</h1>
+      <h2>Total Country Visited: {visitedCountries.length}</h2>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li>{country.name.common}</li>
+        ))}
+      </ol>
       <div className="countries">
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country}></Country>
+          <Country
+            key={country.ccn3.ccn3}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+          ></Country>
         ))}
       </div>
     </div>
